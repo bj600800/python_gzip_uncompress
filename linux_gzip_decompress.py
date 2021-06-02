bbade77c5af087582abaf6c4ff432228ac48e56c
Eric Dou <bj600800@gmail.com>
2021-06-02 19:29:35 +0800

    Update linux_gzip_decompress.py

--- linux_gzip_decompress.py
+++ linux_gzip_decompress.py
@@ -1,14 +1,26 @@
+#!/usr/bin/env python           
+# -*- coding:utf-8 -*-          
+# @Filename:    zipfile.py      
+# @Author:      Eric Dou        
+# @Time:        2021/6/2 15:28 
+
+"""Function: """
+
 import gzip
 import os
 import shutil
 
-path = input('Input abs path.\n')
+path = r'F:\python_script_learning\test'
 filelists = os.listdir(path)
 for i in filelists:
     if os.path.isdir(os.path.join(path,i)):
         dirpath = os.path.join(path,i)
-        gzfile = os.listdir(dirpath)[0]
-        with gzip.open(os.path.join(dirpath,gzfile), 'rb') as f_in:
-            writefile = gzfile.replace('.gz','.pdb')
-            with open(os.path.join(dirpath,writefile), 'wb') as f_out:
-                shutil.copyfileobj(f_in, f_out)
+        gzfile = os.listdir(dirpath)
+        if len(gzfile) == 1:
+            for i in gzfile:
+                with gzip.open(os.path.join(dirpath,i), 'rb') as f_in:
+                    writefile = i.replace('.gz','.pdb')
+                    with open(os.path.join(dirpath,writefile), 'wb') as f_out:
+                        shutil.copyfileobj(f_in, f_out)
+
+
